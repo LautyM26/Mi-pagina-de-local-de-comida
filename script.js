70ecd4af268791dbd6c30664fc120daed803d217
Lautaro Mejias <mejiaslauti@gmail.com>
2024-11-01 21:47:23 -0300

    Entrega final N°2

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
 let nombre
-let pedidos = []
+let pedidos = JSON.parse(localStorage.getItem("pedidos")) || [];
 let productoSeleccionado
 
 const menu = document.getElementById("menu")
 document.getElementById("botonContinuar").onclick = guardarNombre
 
 const productos = [
-    { nombre: "Pizza", precio: 8000 },
-    { nombre: "Hamburguesa", precio: 6000 },
-    { nombre: "Pancho", precio: 3000 },
-    { nombre: "Fideos", precio: 7500 },
-    { nombre: "Sushi", precio: 13000 },
+    { nombre: "Pizza", precio: 8000, stock: 200 },
+    { nombre: "Hamburguesa", precio: 6000, stock: 300 },
+    { nombre: "Pancho", precio: 3000, stock: 500 },
+    { nombre: "Fideos", precio: 7500, stock: 100 },
+    { nombre: "Sushi", precio: 13000, stock: 100 },
 ]
 
-productos.forEach(producto => {
-    const boton = document.createElement("button")
-    boton.innerText = `${producto.nombre} ($${producto.precio})`
-    boton.onclick = () => seleccionarProducto(producto.nombre.toLowerCase())
-    menu.appendChild(boton)
-})
+function inicializarMenu () {
+    const menu = document.getElementById("menu")
 
-{
-    const verPedidoButton = document.createElement("button")
-    verPedidoButton.innerText = "Ver Pedido"
-    verPedidoButton.onclick = verPedido
-    menu.appendChild(verPedidoButton)
+    productos.forEach(producto => {
+        const boton = document.createElement("button")
+        boton.innerText = `${producto.nombre} ($${producto.precio})`
+        boton.onclick = () => seleccionarProducto(producto)
+        menu.appendChild(boton)
+    })
+    
+    {
+        const verPedidoButton = document.createElement("button")
+        verPedidoButton.innerText = "Ver Pedido"
+        verPedidoButton.onclick = verPedido
+        menu.appendChild(verPedidoButton)
+    }
+    
+    {
+       const finalizarPedidoButton = document.createElement("button")
+    
+       finalizarPedidoButton.innerText = "Finalizar Pedido"
+       finalizarPedidoButton.onclick = cuentaFinal
+       menu.appendChild(finalizarPedidoButton)
+    }
+
+    document.getElementById("confirmarPedido").onclick = confirmarPedido
+    document.getElementById("cancelar").onclick = cancelar
 }
 
-{
-   const finalizarPedidoButton = document.createElement("button")
-
-   finalizarPedidoButton.innerText = "Finalizar Pedido"
-   finalizarPedidoButton.onclick = cuentaFinal
-   menu.appendChild(finalizarPedidoButton)
-}
-document.getElementById("confirmarPedido").onclick = confirmarPedido
-document.getElementById("cancelar").onclick = cancelar
+inicializarMenu()
 
 
 function guardarNombre() {
@@ -55,57 +62,37 @@ function seleccionarProducto(producto) {
     document.getElementById("detalle").style.display = "block"
 }
 
-    let mensaje = ""
-    let maxCantidad = 0
-    let precioUnitario = 0
 
-
-    function asignarValores (mensajeProducto, cantidad, precio) {
-        mensaje = mensajeProducto
-        maxCantidad = cantidad
-        precioUnitario = precio
-    }
-
-
-    function asignarProducto(producto) {
-        switch (producto) {
-            case "pizza":
-                asignarValores("¿cuantas pizzas desea ordenar? (stock maximo 300 pizzas)", 300, 8000)
-                break
-            case "hamburguesa":
-                asignarValores("¿cuantas hamburguesas desea ordenar? (stock maximo 200 hamburguesas)", 200, 6000)
-                break
-            case "pancho":
-                asignarValores("¿cuantos panchos desea ordenar? (stock maximo 500 panchos)", 500, 3000)
-                break; 
-            case "fideos":
-                asignarValores("¿cuantas fideos desea ordenar? (stock maximo 100 platos de fideos)", 100, 7500)
-                break
-            case "sushi":
-                asignarValores("¿cuantos sushis desea ordenar? (stock maximo 100 sushis)", 100, 13000)
-                break; 
-            default:
-                mensaje = "Usted eligio un numero o caracter equiocado, porfavor vuelva a seleccionar"
-                maxCantidad = 0;
-                precioUnitario = 0;
-                break
-        }
-    }
-
-document.getElementById("confirmarPedido").onclick = function() {
-    let cantidad = parseInt(document.getElementById("cantidad").value)
-    if (cantidad > 0 && cantidad <= maxCantidad) {
-        let total = cantidad * precioUnitario
-        pedidos.push({producto: productoSeleccionado, cantidad: cantidad, total: total})
-
-        alert(`Muchas gracias, tu pedido de ${cantidad} ${productoSeleccionado}(s) ha sido realizado. Total: $${total}`)
-    } else {
-        alert(`No podemos procesar esa cantidad. Por favor, elige lo que dice la consigna`)
-    }
-    document.getElementById("detalle").style.display = "none"
-    document.getElementById("menu").style.display = "block"
+function asignarValores (mensajeProducto, cantidad, precio) {
+    mensaje = mensajeProducto
+    maxCantidad = cantidad
+    precioUnitario = precio
 }
 
+function asignarProducto(producto) {
+    asignarValores(
+        `¿cuantas unidades de ${producto.nombre} desea ordenar? (stock maximo: ${producto.stock})`, producto.stock, producto.precio
+    );
+}
+
+
+function confirmarPedido() {
+    document.getElementById("confirmarPedido").onclick = function() {
+        let cantidad = parseInt(document.getElementById("cantidad").value)
+        if (cantidad > 0 && cantidad <= maxCantidad) {
+            let total = cantidad * precioUnitario
+            pedidos.push({producto: productoSeleccionado.nombre, cantidad: cantidad, total: total})
+
+            localStorage.setItem("pedidos", JSON.stringify(pedidos));
+    
+            alert(`Muchas gracias, tu pedido de ${cantidad} ${productoSeleccionado.nombre}(s) ha sido realizado. Total: $${total}`)
+        } else {
+            alert(`No podemos procesar esa cantidad. Por favor, elige lo que dice la consigna`)
+        }
+        document.getElementById("detalle").style.display = "none"
+        document.getElementById("menu").style.display = "block"
+    }
+}
 
 function verPedido() {
     let resumenContainer = document.getElementById("resumenPedidos")
